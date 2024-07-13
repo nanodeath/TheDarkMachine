baefab8793f80f4792ecdb6c360753ed22bc2c5e
Max Aller <nanodeath@gmail.com>
2024-07-12 20:50:37 -0700

    Replace soldier animation player with tweens.

--- the-dark-machine/scripts/Soldier.gd
+++ the-dark-machine/scripts/Soldier.gd
@@ -1,19 +1,4 @@
-class_name Solider extends CharacterBody2D
-
-@onready var animation_player = $AnimationPlayer
+class_name Soldier extends CharacterBody2D
 
 func _ready():
-	animation_player.play("walk_left")
-	animation_player.animation_finished.connect(self._animation_finished)
-
-func _animation_finished(anim_name: StringName):
-	# print("Done! " + str(anim_name))
-	if anim_name == 'walk_left':
-		animation_player.play("walk_right")
-	else:
-		animation_player.play("walk_left")
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
---
+++ the-dark-machine/scripts/ai/patrol.gd
@@ -0,0 +1,31 @@
+class_name Patrol extends Node
+
+@export var move_right_initially: bool
+@export var distance: float
+@export var speed: float = 1
+@export var pause_duration: float = 1
+@onready var sprite = $"../Sprite"
+
+func _ready():
+	patrol()
+		
+func patrol():
+	var tween := get_tree().create_tween()
+	tween.set_loops()
+	var parent := get_parent()
+	if move_right_initially:
+		sprite.flip_h = false
+		tween.tween_property(parent, "position", parent.global_position + Vector2(distance, 0), distance / speed)
+		tween.tween_interval(pause_duration)
+		tween.tween_callback(func(): sprite.flip_h = true)
+		tween.tween_property(parent, "position", parent.global_position, distance / speed)
+		tween.tween_interval(pause_duration)
+		tween.tween_callback(func(): sprite.flip_h = false)
+	else:
+		sprite.flip_h = true
+		tween.tween_property(parent, "position", parent.global_position - Vector2(distance, 0), distance / speed)
+		tween.tween_interval(pause_duration)
+		tween.tween_callback(func(): sprite.flip_h = false)
+		tween.tween_property(parent, "position", parent.global_position, distance / speed)
+		tween.tween_interval(pause_duration)
+		tween.tween_callback(func(): sprite.flip_h = true)
